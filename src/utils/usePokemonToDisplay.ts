ad652676c4765c3e4eee7dbdd8a1a4c168c117ff
marboqmar <martamartinezboquer@gmail.com>
2024-07-12 11:06:20 +0200

    If is not searching returns pokemon ordered by number, if is searching by name returns pokemon ordered by if they are set as favourite and then by alphabetical order, and if is searching by id returns pokemon ordered by if they are set as favourite and then by numerical order

--- src/utils/usePokemonToDisplay.ts
+++ src/utils/usePokemonToDisplay.ts
@@ -2,28 +2,41 @@ import { useContext, useMemo } from "react";
 import { FiltersContext } from "../contexts/FiltersContextProvider.tsx";
 import { pokemonDetailsParsed } from "../models/pokemonInfo.ts";
 import { InfoFromApiContext } from "../contexts/InfoFromApiContextProvider.tsx";
+import { orderBy } from "lodash";
 
 export const usePokemonToDisplay = () => {
   const { parsedPokemon } = useContext(InfoFromApiContext);
   const { search, isOnlyFavs } = useContext(FiltersContext);
 
   return useMemo(() => {
-    return parsedPokemon.filter((pokemon: pokemonDetailsParsed) => {
-      if (isOnlyFavs && !pokemon.isFav) {
-        return false;
-      }
+    const filteredPokemon = parsedPokemon.filter(
+      (pokemon: pokemonDetailsParsed) => {
+        if (isOnlyFavs && !pokemon.isFav) {
+          return false;
+        }
 
-      if (!search) {
-        return true;
-      }
+        if (!search) {
+          return true;
+        }
 
-      const searchId = Number(search);
+        const searchId = Number(search);
 
-      if (Number.isNaN(searchId)) {
-        return pokemon.name.includes(search);
-      }
+        if (Number.isNaN(searchId)) {
+          return pokemon.name.includes(search);
+        }
 
-      return pokemon.id.toString().includes(searchId.toString());
-    });
+        return pokemon.id.toString().includes(searchId.toString());
+      },
+    );
+
+    if (!search) {
+      return filteredPokemon;
+    }
+
+    if (Number(search)) {
+      return orderBy(filteredPokemon, ["isFav", "id"], ["desc", "asc"]);
+    }
+
+    return orderBy(filteredPokemon, ["isFav", "name"], ["desc", "asc"]);
   }, [search, isOnlyFavs, parsedPokemon]);
 };
