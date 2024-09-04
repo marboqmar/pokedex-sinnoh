055968a84112bc2c46cbfc2f4c0e6dd710e12b86
marboqmar <martamartinezboquer@gmail.com>
2024-09-04 12:44:05 +0200

    Modified pagination to not show all available pages

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -26,38 +26,55 @@ export const Pagination = ({
     setNewPaginationNumbers(paginationNumbersList);
   }, [length, pokemonPerPage, setNewPaginationNumbers]);
 
-  const determineCurrentPage = useCallback(
-    (page: number) => {
-      const lastPage: number = paginationNumbers.slice(-1)[0];
+  const lastAvailablePage: number = paginationNumbers.slice(-1)[0];
 
-      if (page > lastPage) {
+  // When clicking on DisplayOnlyFavsButton it did not recalculate currentPage,
+  // so if you clicked DisplayOnlyFavsButton while on page 2 with only one liked pokemon, you would be on a non existing
+  // currentPage 2 and had to click page 1 to see the pokemon.
+
+  // This function sets current page to 1 when current page is greater than the available pages (e.g. if your current
+  // page is 6 and click on DisplayOnlyFavsButton and you have only 1 page, then it will set your current page to 1)
+  const determineCurrentPage = useCallback(
+    (currentPage: number) => {
+      if (currentPage > lastAvailablePage) {
         setNewCurrentPage(1);
       } else {
-        setNewCurrentPage(page);
+        setNewCurrentPage(currentPage);
       }
     },
-    [paginationNumbers, setNewCurrentPage],
+    [setNewCurrentPage, lastAvailablePage],
   );
 
-  // When clicking on DisplayOnlyFavsButton, it did re-render paginationNumbers, but did not recalculate currentPage,
-  // so if you clicked DisplayOnlyFavsButton while on page 2 with only one liked pokemon, you would be on a non existing
-  // currentPage 2 and had to click page 1 to see the pokemon. This useEffect recalculates currentPage when
-  // paginationNumbers changes
+  //This useEffect recalculates currentPage when paginationNumbers changes
   useEffect(() => {
     determineCurrentPage(currentPage);
   }, [currentPage, determineCurrentPage, paginationNumbers]);
 
   return (
     <div className="flex justify-center gap-12 pagination">
-      {paginationNumbers.map((pageNumber) => (
+      {currentPage === 1 ? (
+        <></>
+      ) : (
         <Button
-          className={`pagination__button ${currentPage === pageNumber ? "pagination__button--active" : ""}`}
-          key={pageNumber}
-          onClick={() => determineCurrentPage(pageNumber)}
+          className={"pagination__button"}
+          onClick={() => determineCurrentPage(currentPage - 1)}
         >
-          {pageNumber}
+          <i className="fa-solid fa-chevron-left"></i>
         </Button>
-      ))}
+      )}
+      <div className={"pagination__button pagination__current-page"}>
+        {currentPage}
+      </div>
+      {lastAvailablePage === currentPage ? (
+        <></>
+      ) : (
+        <Button
+          className={"pagination__button"}
+          onClick={() => determineCurrentPage(currentPage + 1)}
+        >
+          <i className="fa-solid fa-chevron-right"></i>
+        </Button>
+      )}
     </div>
   );
 };
